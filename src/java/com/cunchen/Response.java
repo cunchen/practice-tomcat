2dc42a03d89c13dd2bdaeda2db2e0641a61f015f
WangQidong <wang135139@gmail.com>
2017-02-13 19:04:33 +0800

    静态解析动态解析修复 Servlet项目输出加载

---
+++ src/java/com/cunchen/Response.java
@@ -0,0 +1,8 @@
+package com.cunchen;
+
+/**
+ * 服务器内部Response接口
+ * Created by wqd on 2017/2/13.
+ */
+public interface Response {
+}
---
+++ src/java/com/cunchen/connector/ResponseBase.java
@@ -0,0 +1,93 @@
+package com.cunchen.connector;
+
+import com.cunchen.Response;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Locale;
+
+/**
+ * ResponseBase实现
+ * Created by wqd on 2017/2/13.
+ */
+public class ResponseBase implements Response, ServletResponse {
+
+    private ResponseStream responseStream ;
+
+    @Override
+    public String getCharacterEncoding() {
+        return null;
+    }
+
+    @Override
+    public String getContentType() {
+        return null;
+    }
+
+    @Override
+    public ServletOutputStream getOutputStream() throws IOException {
+        return null;
+    }
+
+    @Override
+    public PrintWriter getWriter() throws IOException {
+        return null;
+    }
+
+    @Override
+    public void setCharacterEncoding(String s) {
+
+    }
+
+    @Override
+    public void setContentLength(int i) {
+
+    }
+
+    @Override
+    public void setContentType(String s) {
+
+    }
+
+    @Override
+    public void setBufferSize(int i) {
+
+    }
+
+    @Override
+    public int getBufferSize() {
+        return 0;
+    }
+
+    @Override
+    public void flushBuffer() throws IOException {
+
+    }
+
+    @Override
+    public void resetBuffer() {
+
+    }
+
+    @Override
+    public boolean isCommitted() {
+        return false;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void setLocale(Locale locale) {
+
+    }
+
+    @Override
+    public Locale getLocale() {
+        return null;
+    }
+}
